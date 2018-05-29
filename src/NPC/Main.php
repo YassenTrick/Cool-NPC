c2c78942dc12500d0434448f116fd6d38bfe3bf7
FreeGamingHere <freegaminghere@gmail.com>
2018-05-29 16:16:49 +0300

    Added maths

--- src/NPC/Main.php
+++ src/NPC/Main.php
@@ -7,6 +7,7 @@ namespace NPC;
 use pocketmine\Player;
 use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
 use pocketmine\command\{
 	Command, CommandSender
@@ -17,7 +18,7 @@ use pocketmine\utils\TextFormat;
 use NPC\tasks\NPCTask;
 
 class Main extends PluginBase implements Listener{
-
+	
 	public function onEnable(){
 		if(!is_dir($this->getDataFolder())){
 			@mkdir($this->getDataFolder());
@@ -25,7 +26,7 @@ class Main extends PluginBase implements Listener{
 		
 		$this->getLogger()->info("Enabled!");
 		
-		$config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML);
+		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 		if($config->get("sneak")){
 			$this->getLogger()->info("Enabled sneaking");
 		}
@@ -35,30 +36,69 @@ class Main extends PluginBase implements Listener{
 		if($config->get("particles")){
 			$this->getLogger()->info("Enabled particles");
 		}
+		if($config->get("maths")){
+			$this->getLogger()->info("Enabled maths");
+		}
 		$this->saveResource("config.yml");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		Entity::registerEntity(NPCHuman::class, true);
 	}
-
+	
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
 		if(count($args) < 1){
 			$sender->sendMessage("Usage: /npc <name>");
 			return false;
 		}
-
+		
 		$this->spawnNPC($sender, $args[0]);
 		$sender->sendMessage(TextFormat::GREEN . "Spawned NPC: " . $args[0]);
 		return true;
 	}
-
+	
+	public function onChat(PlayerChatEvent $event){
+		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		if($config->get("maths")){
+			$msg = $event->getMessage();
+			$player = $event->getPlayer();
+			$prefix = $config->get("prefix");
+			if($msg[0] == $prefix){
+				foreach($this->getServer()->getOnlinePlayers() as $p){
+					if(!is_numeric($msg[1]) or !is_numeric($msg[3]) or $msg[2] !== "+" or $msg[2] !== "-" or $msg[2] !== "*" or $msg[2] !== "/"){
+						$player->sendMessage(TextFormat::GREEN . "[Bot]" . TextFormat::RED . "Usage: " . $prefix . " {number} + or - or * or / {number}");
+						$event->setCancelled();
+						return;
+					} else {
+						if($msg[2] == "+"){
+							$p->sendMessage(TextFormat::GREEN . "[Bot]" . TextFormat::BLUE . $msg[1] . " plus " . $msg[3] . " equals " . (int)$msg[1] + (int)$msg[3]);
+							return;
+						} elseif($msg[2] == "-"){
+							$p->sendMessage(TextFormat::GREEN . "[Bot]" . TextFormat::BLUE . $msg[1] . " minus " . $msg[3] . " equals " . (int)$msg[1] - (int)$msg[3]);
+							return;
+						} elseif($msg[2] == "*"){
+							$p->sendMessage(TextFormat::GREEN . "[Bot]" . TextFormat::BLUE . $msg[1] . " times " . $msg[3] . " equals " . (int)$msg[1] * (int)$msg[3]);
+							return;
+						} elseif($msg[2] == "/"){
+							$p->sendMessage(TextFormat::GREEN . "[Bot]" . TextFormat::BLUE . $msg[1] . " devided by " . $msg[3] . " equals " . (int)$msg[1] / (int)$msg[3]);
+							return;
+						}
+					}
+					if($msg == "2 + 2 - 1"){
+						$p->sendMessage(TextFormat::GREEN . "[Big Shaq]" . TextFormat::BLUE . "Two plus two is four, minus one that's three quick maths.");
+						return;
+					}
+				}
+			}
+		} 
+	}
+	
 	public function onEntitySpawn(EntitySpawnEvent $e){
 		$entity = $e->getEntity();
-
+		
 		if($entity instanceof NPCHuman){
 			$this->getServer()->getScheduler()->scheduleRepeatingTask(new NPCTask($this, $entity), 200);
 		}
 	}
-
+	
 	public function spawnNPC(Player $player, string $name){
 		$nbt = Entity::createBaseNBT($player, null, 2, 2);
 		$nbt->setTag($player->namedtag->getTag("Skin"));
